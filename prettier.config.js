891f70cda675ddbe74ea037028db0b6d8f9716c5
MacbookAir <florent2303@pm.me>
2024-05-27 15:29:45 +0200

    feat : nextjs tutorial

--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,15 @@
-const styleguide = require('@vercel/style-guide/prettier');
+const styleguide = require('@vercel/style-guide/prettier')
+const config = {
+  trailingComma: 'es5',
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  printWidth: 90,
+  bracketSameLine: true,
+}
 
 module.exports = {
+  ...config,
   ...styleguide,
   plugins: [...styleguide.plugins, 'prettier-plugin-tailwindcss'],
-};
+}
